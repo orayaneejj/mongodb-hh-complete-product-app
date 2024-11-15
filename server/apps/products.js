4709552361bbe6246c213d8d536d72a97ff72f48
Orayanee Janjaeng <orayaneejj@gmail.com>
2024-11-15 16:56:42 +0700

    feat: add status codes and error handlings for api

--- server/apps/products.js
+++ server/apps/products.js
@@ -1,53 +1,114 @@
 import { Router } from "express";
 import { db } from "../utils/db.js";
 import { ObjectId } from "mongodb";
+
 const productRouter = Router();
 
+// get all products
 productRouter.get("/", async (req, res) => {
-  const collection = db.collection("products");
-  const products = await collection.find({}).toArray();
-  return res.json({
-    data: products,
-  });
+  try {
+    const collection = db.collection("products");
+    const products = await collection.find({}).toArray();
+    return res.status(200).json({
+      data: products,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 });
 
+// get product by ID
 productRouter.get("/:id", async (req, res) => {
-  const collection = db.collection("products");
-  const productID = new ObjectId(req.params.id);
-  const product = await collection.findOne({ _id: productID });
-  return res.json({
-    data: product,
-  });
+  try {
+    const collection = db.collection("products");
+    const productID = new ObjectId(req.params.id);
+    const product = await collection.findOne({ _id: productID });
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+
+    return res.status(200).json({
+      data: product,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 });
 
+// post new product
 productRouter.post("/", async (req, res) => {
-  const collection = db.collection("products");
-  const productData = { ...req.body };
-  const products = await collection.insertOne(productData);
-  return res.json({
-    message: "Product has been created successfully",
-  });
+  try {
+    const collection = db.collection("products");
+    const productData = { ...req.body };
+    const result = await collection.insertOne(productData);
+
+    return res.status(201).json({
+      message: "Product has been created successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 });
 
+// put product by ID
 productRouter.put("/:id", async (req, res) => {
-  const collection = db.collection("products");
-  const productID = new ObjectId(req.params.id);
-  const newProductData = { ...req.body };
-  await collection.updateOne({ _id: productID }, { $set: newProductData });
-  return res.json({
-    message: "Product has been updated successfully",
-  });
+  try {
+    const collection = db.collection("products");
+    const productID = new ObjectId(req.params.id);
+    const newProductData = { ...req.body };
+
+    const result = await collection.updateOne(
+      { _id: productID },
+      { $set: newProductData }
+    );
+
+    if (result.matchedCount === 0) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Product has been updated successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 });
 
+// delete product by ID
 productRouter.delete("/:id", async (req, res) => {
-  const collection = db.collection("products");
-  const productID = new ObjectId(req.params.id);
-  await collection.deleteOne({
-    _id: productID,
-  });
-  return res.json({
-    message: "Product has been deleted successfully",
-  });
+  try {
+    const collection = db.collection("products");
+    const productID = new ObjectId(req.params.id);
+
+    const result = await collection.deleteOne({ _id: productID });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Product has been deleted successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 });
 
 export default productRouter;
--- server/utils/db.js
+++ server/utils/db.js
@@ -4,6 +4,5 @@ const connectionString = "mongodb://127.0.0.1:27017";
 export const client = new MongoClient(connectionString, {
   useUnifiedTopology: true,
 });
-await client.connect();
 
 export const db = client.db("practice-mongo");
