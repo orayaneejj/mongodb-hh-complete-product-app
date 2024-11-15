e833cee2a602dbc81b7238fb79408e793e3b2536
Orayanee Janjaeng <orayaneejj@gmail.com>
2024-11-15 14:42:51 +0700

    feat: add CRUD api

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,1 +1,9 @@
 // Set up db connection here
+import { MongoClient } from "mongodb";
+const connectionString = "mongodb://127.0.0.1:27017";
+export const client = new MongoClient(connectionString, {
+  useUnifiedTopology: true,
+});
+await client.connect();
+
+export const db = client.db("practice-mongo");
